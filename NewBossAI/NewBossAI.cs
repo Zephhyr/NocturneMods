474e76d6cb7f3d8a36523448d224135759f8c1ca
Zephhyr <alpha452@live.co.uk>
2023-06-29 13:57:42 +0100

    WIP new AI 2

--- NewBossAI/NewBossAI.cs
+++ NewBossAI/NewBossAI.cs
@@ -6,6 +6,9 @@ using Il2Cppnewdata_H;
 using Il2Cppresult2_H;
 using Il2Cppnewbattle_H;
 using Il2Cppeffect_H;
+using MelonLoader.CoreClrUtils;
+using static UnityEngine.ScriptingUtility;
+using System.Diagnostics;
 
 [assembly: MelonInfo(typeof(NewBossAI.NewBossAI), "New Boss AI", "1.0.0", "Zephhyr")]
 [assembly: MelonGame("", "smt3hd")]
@@ -15,23 +18,17 @@ namespace NewBossAI
     internal class NewBossAI : MelonMod
     {
         private static Random random = new Random();
-        private static ushort currentBattleTurnCount;
-        private static ushort currentBattleActionCount;
-        private static ushort currentTurnActionCount;
-        private static List<ushort> skillsUsedThisBattle = new List<ushort>();
-        private static List<ushort> skillsUsedThisTurn = new List<ushort>();
+        private static Dictionary<int, ActionTracker> actionTrackers = new Dictionary<int, ActionTracker>();
+        private static Dictionary<int, short> extraTurns = new Dictionary<int, short>();
 
         public override void OnInitializeMelon()
         {
-            datDevilFormat.tbl[256].maxhp = 600;
-            datDevilFormat.tbl[256].hp = 600;
-            
-            datNormalSkill.tbl[81] = datNormalSkill.tbl[219];
-            OverWriteSkillEffect(81, 219);
+            BossForneus();
+            NewBeastEye();
         }
 
         [HarmonyPatch(typeof(datSkillName), nameof(datSkillName.Get))]
-        private class Patch
+        private class SkillNamesPatch
         {
             public static void Postfix(ref int id, ref string __result)
             {
@@ -43,23 +40,33 @@ namespace NewBossAI
                 }
             }
         }
-
-        //[HarmonyPatch(typeof(nbActionProcess), nameof(nbActionProcess.SetAction_READY))]
-        //private class ActionProcessPatch1
-        //{
-        //    public static void Prefix(ref nbActionProcessData_t a)
-        //    {
-        //        MelonLogger.Msg("-SetAction_READY-");
-        //    }
-        //}
-        //[HarmonyPatch(typeof(nbActionProcess), nameof(nbActionProcess.SetAction_SKILL))]
-        //private class ActionProcessPatch
-        //{
-        //    public static void Prefix(ref nbActionProcessData_t a)
-        //    {
-        //        MelonLogger.Msg("-SetAction_SKILL-");
-        //    }
-        //}
+        [HarmonyPatch(typeof(nbActionProcess), nameof(nbActionProcess.SetAddPressPacket))]
+        private class PressPatch1
+        {
+            public static void Postfix(ref nbActionProcessData_t a, ref int nskill)
+            {
+                if (a.newaddpresstype == 15 && nskill == 81)
+                {
+                    a.newaddpresstype = 18;
+                    if (!extraTurns.ContainsKey(a.work.id))
+                        extraTurns.Add(a.work.id, 1);
+                    else
+                        extraTurns[a.work.id] += 1;
+                }
+            }
+        }
+        [HarmonyPatch(typeof(nbMakePacket), nameof(nbMakePacket.nbMakeNewPressPacket))]
+        private class PressPatch2
+        {
+            public static void Postfix(ref int startframe, ref int uniqueid, ref int ptype, ref nbFormation_t form)
+            {
+                if (ptype == 18)
+                {
+                    nbMakePacket.nbAddNewPressPacket(startframe, uniqueid, 1, 1);
+                    nbHelpProcess.nbDispText("Turn Count Increased!", string.Empty, 2, 45, 2315190144, false);
+                }
+            }
+        }
 
         //------------------------------------------------------------
 
@@ -68,9 +75,8 @@ namespace NewBossAI
         {
             public static void Postfix()
             {
-                currentBattleTurnCount = 0;
-                currentBattleActionCount = 0;
-                skillsUsedThisBattle.Clear();
+                actionTrackers.Clear();
+                extraTurns.Clear();
                 MelonLogger.Msg("-Battle Starts-");
             }
         }
@@ -80,13 +86,33 @@ namespace NewBossAI
         {
             public static void Postfix()
             {
-                short activeUnit = nbMainProcess.nbGetMainProcessData().activeunit;
+                var mainProcessData = nbMainProcess.nbGetMainProcessData();
+                short activeUnit = mainProcessData.activeunit;
 
                 if (activeUnit >= 4)
                 {
-                    currentBattleTurnCount++;
-                    currentTurnActionCount = 0;
-                    skillsUsedThisTurn.Clear();
+                    var enemyUnits = mainProcessData.enemyunit.Where(x => x.id != 0);
+                    foreach (var unit in enemyUnits)
+                    {
+                        if (!actionTrackers.ContainsKey(unit.id))
+                        {
+                            actionTrackers.Add(unit.id, new ActionTracker());
+                        }
+
+                        if (extraTurns.ContainsKey(unit.id))
+                        {
+                            nbMainProcess.nbGetMainProcessData().press4_p += extraTurns[unit.id];
+                            nbMainProcess.nbGetMainProcessData().press4_ten += extraTurns[unit.id];
+                        }
+                    }
+                    
+                    foreach (var actionCounter in actionTrackers.Values)
+                    {
+                        actionCounter.currentBattleTurnCount++;
+                        actionCounter.currentTurnActionCount = 0;
+                        actionCounter.skillsUsedThisTurn.Clear();
+                    }
+
                     MelonLogger.Msg("-Enemy Turn Starts-");
                 }
             }
@@ -97,33 +123,34 @@ namespace NewBossAI
         {
             public static void Prefix(ref nbActionProcessData_t a, ref int code, ref int n)
             {
-                currentBattleActionCount++;
-                currentTurnActionCount++;
+                actionTrackers[a.work.id].currentBattleActionCount++;
+                actionTrackers[a.work.id].currentTurnActionCount++;
+
                 if (a.work.id >= 256)
-                    RunNewBossAI(ref a);
+                    RunNewBossAI(ref a, ref code, ref n);
             }
         }
 
-        private static void RunNewBossAI(ref nbActionProcessData_t a)
+        private static void RunNewBossAI(ref nbActionProcessData_t a, ref int code, ref int n)
         {
             switch (a.work.id)
             {
-                case 256: RunNewForneusAI(ref a); break;
+                case 256: RunNewForneusAI(ref a, ref code, ref n); break;
                 default: break;
             }
         }
 
-        private static void RunNewForneusAI(ref nbActionProcessData_t a)
+        private static void RunNewForneusAI(ref nbActionProcessData_t a, ref int code, ref int n)
         {
-            MelonLogger.Msg("currentBattleTurnCount:" + currentBattleTurnCount);
-            MelonLogger.Msg("currentBattleActionCount:" + currentBattleActionCount);
-            MelonLogger.Msg("currentTurnActionCount:" + currentTurnActionCount);
+            MelonLogger.Msg("currentBattleTurnCount:" + actionTrackers[a.work.id].currentBattleTurnCount);
+            MelonLogger.Msg("currentBattleActionCount:" + actionTrackers[a.work.id].currentBattleActionCount);
+            MelonLogger.Msg("currentTurnActionCount:" + actionTrackers[a.work.id].currentTurnActionCount);
             MelonLogger.Msg("-Action Starts-");
             ushort currentHpPercent = BossCurrentHpPercent(ref a);
             MelonLogger.Msg("Forneus HP%: " + currentHpPercent);
             MelonLogger.Msg("Forneus HP: " + a.work.hp);
             
-            if (!skillsUsedThisBattle.Contains(81))
+            if (!actionTrackers[a.work.id].skillsUsedThisBattle.Contains(81))
             {
                 UseSkill(ref a, 81); return;
             }
@@ -139,10 +166,8 @@ namespace NewBossAI
             }
             else if (currentHpPercent > 20)
             {
-                if (currentTurnActionCount == 1 && !skillsUsedThisTurn.Contains(219))
-                    UseSkill(ref a, 219);
-                else if (!skillsUsedThisBattle.Contains(244))
-                    UseSkill(ref a, 244);
+                if (!actionTrackers[a.work.id].skillsUsedThisBattle.Contains(10))
+                    UseSkill(ref a, 10);
                 else
                 {
                     int randomValue = random.Next(2);
@@ -155,10 +180,10 @@ namespace NewBossAI
             }
             else
             {
-                if (currentTurnActionCount == 1 && !skillsUsedThisTurn.Contains(219))
+                if (actionTrackers[a.work.id].currentTurnActionCount == 1 && !actionTrackers[a.work.id].skillsUsedThisTurn.Contains(219))
                     UseSkill(ref a, 219);
-                else if (!skillsUsedThisBattle.Contains(272))
-                    UseSkill(ref a, 272);
+                else if (!actionTrackers[a.work.id].skillsUsedThisBattle.Contains(244))
+                    UseSkill(ref a, 244);
                 else
                 {
                     int randomValue = random.Next(2);
@@ -181,8 +206,9 @@ namespace NewBossAI
         {
             a.work.nowcommand = 1;
             a.work.nowindex = skillId;
-            if (!skillsUsedThisBattle.Contains(skillId)) skillsUsedThisBattle.Add(skillId);
-            if (!skillsUsedThisTurn.Contains(skillId)) skillsUsedThisTurn.Add(skillId);
+
+            if (!actionTrackers[a.work.id].skillsUsedThisBattle.Contains(skillId)) actionTrackers[a.work.id].skillsUsedThisBattle.Add(skillId);
+            if (!actionTrackers[a.work.id].skillsUsedThisTurn.Contains(skillId)) actionTrackers[a.work.id].skillsUsedThisTurn.Add(skillId);
         }
 
         private static ushort BossCurrentHpPercent(ref nbActionProcessData_t a)
@@ -194,11 +220,50 @@ namespace NewBossAI
 
         //------------------------------------------------------------
 
+        private static void BossForneus()
+        {
+            datDevilFormat.tbl[256].maxhp = 800;
+            datDevilFormat.tbl[256].hp = 800;
+            datDevilAI.divTbls[2][0].ailevel = 1;
+        }
+
         private static void OverWriteSkillEffect(ushort targetId, ushort originId)
         {
             datNormalSkillVisual.tbl[targetId] = datNormalSkillVisual.tbl[originId];
             nbActionProcess.sobedtbl[targetId] = nbActionProcess.sobedtbl[originId];
             nbCamera_SkillPtrTable.tbl[targetId] = nbCamera_SkillPtrTable.tbl[originId];
+        }
+
+        private static void NewBeastEye()
+        {
+            datSkill.tbl[81].keisyoform = 512;
+            datSkill.tbl[81].skillattr = 5;
+            datNormalSkill.tbl[81].hojotype = 4096;
+            datNormalSkill.tbl[81].hojopoint = 2;
+            datNormalSkill.tbl[81].hpbase = 0;
+            datNormalSkill.tbl[81].hpn = 50;
+            datNormalSkill.tbl[81].hptype = 0;
+            datNormalSkill.tbl[81].program = 13;
+            datNormalSkill.tbl[81].use = 2;
+            OverWriteSkillEffect(81, 219);
+        }
+    }
+
+    internal class ActionTracker
+    {
+        public ushort currentBattleTurnCount;
+        public ushort currentBattleActionCount;
+        public ushort currentTurnActionCount;
+        public List<ushort> skillsUsedThisBattle;
+        public List<ushort> skillsUsedThisTurn;
+
+        public ActionTracker()
+        {
+            this.currentBattleTurnCount = 0;
+            this.currentBattleActionCount = 0;
+            this.currentTurnActionCount = 0;
+            this.skillsUsedThisBattle = new List<ushort>();
+            this.skillsUsedThisTurn = new List<ushort>();
         }
     }
 }
