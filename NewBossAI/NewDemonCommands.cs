4068d24a67ef4af0eb129e6c92fc357ba990c58e
Zephhyr <alpha452@live.co.uk>
2023-07-01 23:56:16 +0100

    integrated Demon Self-Switching and Demon Item Use

---
+++ NewBossAI/NewDemonCommands.cs
@@ -0,0 +1,69 @@
+﻿using HarmonyLib;
+using MelonLoader;
+using Il2Cppnewbattle_H;
+using System.Collections.Generic;
+using System.Linq;
+using Il2Cpp;
+
+namespace NewBossAI
+{
+    internal partial class NewBossAI : MelonMod
+    {
+        [HarmonyPatch(typeof(nbCommSelProcess), nameof(nbCommSelProcess.DispCommandList2))]
+        private class NewCommandsPatch
+        {
+            public static void Prefix(ref nbCommSelProcessData_t s, ref int type, ref int ox, ref int oy, ref int a)
+            {
+                if (s.my.formindex != 0)
+                {
+                    // Self-Switch
+                    var party = s.data.party;
+                    var partyIndices = new List<short>();
+                    for (ushort i = 0; i <= 3; i++)
+                        if (party[i].statindex != 0)
+                            partyIndices.Add(party[i].statindex);
+
+                    var stock = dds3GlobalWork.DDS3_GBWK.stocklist.Distinct();
+                    if (stock.Count() > (dds3GlobalWork.DDS3_GBWK.unitwork.Where(x => x.id != 0).Count() + 1))
+                        stock = stock.SkipLast(stock.Count() - (dds3GlobalWork.DDS3_GBWK.unitwork.Where(x => x.id != 0).Count() + 1));
+
+                    var stockIndices = new List<ushort>();
+                    foreach (var i in stock.Where(x => x != 0))
+                        if (!partyIndices.Contains((short)i))
+                            stockIndices.Add((ushort)i);
+
+                    var stockCommands = new ushort[288];
+                    for (ushort i = 0; i < stockIndices.Count; i++)
+                        stockCommands[i] = stockIndices[i];
+
+                    s.commlist[3] = stockCommands;
+                    s.commcnt[3] = stockIndices.Count;
+
+                    // Use Items
+                    var items = dds3GlobalWork.DDS3_GBWK.item;
+                    var itemIndices = new List<ushort>();
+                    for (ushort i = 0; i <= 56; i++)
+                        if (items[i] != 0 && datItem.tbl[i].use >= 2 && datItem.tbl[i].skillid != 72)
+                            itemIndices.Add(i);
+
+                    var itemCommands = new ushort[288];
+                    for (ushort i = 0; i < itemIndices.Count; i++)
+                        itemCommands[i] = itemIndices[i];
+
+                    s.commlist[2] = itemCommands;
+                    s.commcnt[2] = itemIndices.Count;
+                }
+            }
+        }
+
+        [HarmonyPatch(typeof(nbTarSelProcess), nameof(nbTarSelProcess.nbSetTargetProcess))]
+        private class NewCommandsPatch1
+        {
+            public static void Prefix(nbActionProcessData_t act, ref int partyindex, ref int ctype, ref int crule, ref int carea)
+            {
+                if (partyindex != 0 && ctype == 0 && (crule == 12 || crule == 2) && carea == 1 && act.work.nowcommand == 6)
+                    crule = 1;
+            }
+        }
+    }
+}
