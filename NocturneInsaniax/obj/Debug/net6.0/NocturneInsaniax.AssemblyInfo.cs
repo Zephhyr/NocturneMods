4f2b8577919a95b37732853ebe1828fddafc01f1
Zephhyr <alpha452@live.co.uk>
2025-12-26 12:25:38 +0000

    fix item drop luck mult

--- NocturneInsaniax/obj/Debug/net6.0/NocturneInsaniax.AssemblyInfo.cs
+++ NocturneInsaniax/obj/Debug/net6.0/NocturneInsaniax.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NocturneInsaniax")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f7da2eeac19e69ac82a246096f2f824de4b87f8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e90066094e212323e5db2b70489d4823903613cc")]
 [assembly: System.Reflection.AssemblyProductAttribute("NocturneInsaniax")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NocturneInsaniax")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
