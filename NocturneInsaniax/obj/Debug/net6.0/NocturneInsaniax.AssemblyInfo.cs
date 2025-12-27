a2c35aeaa46ea56b616497bda775d69ac46be0ef
Zephhyr <alpha452@live.co.uk>
2025-12-27 14:34:14 +0000

    fixed bosses not being exempt from Stone status

--- NocturneInsaniax/obj/Debug/net6.0/NocturneInsaniax.AssemblyInfo.cs
+++ NocturneInsaniax/obj/Debug/net6.0/NocturneInsaniax.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NocturneInsaniax")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a514505038b304b74e78a4783d40a8534c75882")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2dfac818dca9b6be37304a57e955e6fa4cc3f962")]
 [assembly: System.Reflection.AssemblyProductAttribute("NocturneInsaniax")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NocturneInsaniax")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
