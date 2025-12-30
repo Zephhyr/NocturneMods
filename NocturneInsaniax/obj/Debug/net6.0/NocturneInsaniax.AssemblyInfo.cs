8c07a353d71676c01a9efe2dd5eefbcbd2b97f30
Zephhyr <alpha452@live.co.uk>
2025-12-30 17:53:59 +0000

    fixed compendium bug and incense

--- NocturneInsaniax/obj/Debug/net6.0/NocturneInsaniax.AssemblyInfo.cs
+++ NocturneInsaniax/obj/Debug/net6.0/NocturneInsaniax.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NocturneInsaniax")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1495417589f4d0b63dbf22906929328077b78672")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5a2f8caee4db00837eb1c25155e97332ec5de93")]
 [assembly: System.Reflection.AssemblyProductAttribute("NocturneInsaniax")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NocturneInsaniax")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
