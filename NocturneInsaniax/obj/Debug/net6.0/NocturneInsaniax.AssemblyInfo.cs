f8842d1aea7f9a69b458bc316589adf4854d70cf
Zephhyr <alpha452@live.co.uk>
2026-01-11 13:44:07 +0000

    fixed Noah AI

--- NocturneInsaniax/obj/Debug/net6.0/NocturneInsaniax.AssemblyInfo.cs
+++ NocturneInsaniax/obj/Debug/net6.0/NocturneInsaniax.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NocturneInsaniax")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4cb0fc848fb486e74692da3520a5ce1146b3fbd1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0bfe1c50c1c4c3edc6b6788c17122e1d6e5b1cb")]
 [assembly: System.Reflection.AssemblyProductAttribute("NocturneInsaniax")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NocturneInsaniax")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
