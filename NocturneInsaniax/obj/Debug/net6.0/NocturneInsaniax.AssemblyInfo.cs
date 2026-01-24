9302cb9acc2ceb290b55cc352ef0f849c14c9691
Zephhyr <alpha452@live.co.uk>
2026-01-24 14:30:17 +0000

    fixed Retributive Zeal bug

--- NocturneInsaniax/obj/Debug/net6.0/NocturneInsaniax.AssemblyInfo.cs
+++ NocturneInsaniax/obj/Debug/net6.0/NocturneInsaniax.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NocturneInsaniax")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3a2eaef154f3211fcb9543c538bdab3894fefdd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+060dcfe5a2d9a135b095579c5d289f8563888d55")]
 [assembly: System.Reflection.AssemblyProductAttribute("NocturneInsaniax")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NocturneInsaniax")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
